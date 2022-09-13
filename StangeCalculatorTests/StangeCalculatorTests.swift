b80e5d70d6451758e5c2bf4148109a7ade68572d
ungchun <leedool3003@naver.com>
2022-09-13 15:06:27 +0900

    유닛테스트 기본 사용법 연습

---
+++ StangeCalculatorTests/StangeCalculatorTests.swift
@@ -0,0 +1,45 @@
+//
+//  StangeCalculatorTests.swift
+//  StangeCalculatorTests
+//
+//  Created by 김성훈 on 2022/09/13.
+//
+
+import XCTest
+@testable import UnitTestPractice
+
+class StangeCalculatorTests: XCTestCase {
+    
+    var sut: StrangeCalculator!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = StrangeCalculator()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+    
+    func testExample() throws {
+        
+    }
+    
+    func testPerformanceExample() throws {
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func test_addNumbers호출시_3_7_23을전달했을때_33을반환하는지() {
+        // given
+        let input = [3, 7, 23]
+        
+        // when
+        let result = sut.addNumbers(of: input)
+        
+        // then
+        XCTAssertEqual(result, 33) // 테스트의 결과를 확인하는 함수
+    }
+}
